b2cc1e5e6141a1cd3bee3c0583cfd16b7cc11220
Francisco Javier Torres <elodinelarcano@protonmail.com>
2025-09-16 18:49:58 +0200

    Added Navbar and Header components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar () {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
