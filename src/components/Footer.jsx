93a06130b888028613c3266e9e9925b8c686d0ca
Francisco Javier Torres <elodinelarcano@protonmail.com>
2025-09-16 22:10:59 +0200

    icons updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,46 @@
+import locationPin from '../icons/location-pin.svg'
 export default function Footer () {
   return (
     <footer>
-      <p>Copyright © 2025 Javier Torres</p>
+      <div className='info-and-links'>
+        <div className='info'>
+          <p>Francisco Javier Torres Franco</p>
+          <p>
+            Desarrollador Fullstack, apasionado por la tecnología y el desarrollo de software.
+            Disponible para trabajo de tiempo completo.
+          </p>
+          <a href='mailto:javiertf2000@gmail.com'>javiertf2000@gmail.com</a>
+          <p>
+            <img src={locationPin} alt='Location' />
+            Lugo, Galicia, España
+          </p>
+        </div>
+        <div className='quick-links'>
+          <h3>Quick Links</h3>
+          <ul>
+            <li>Sobre mí</li>
+            <li>Skills</li>
+            <li>Projectos</li>
+            <li>Contacto</li>
+          </ul>
+        </div>
+      </div>
+      <div className='featured-projects'>
+        <h3>Featured Projects</h3>
+        <ul>
+          <li>Project 1</li>
+          <li>Project 2</li>
+          <li>Project 3</li>
+        </ul>
+      </div>
+      <hr />
+      <div>
+        <p>Copyright © {new Date().getFullYear()} Francisco Javier Torres Franco</p>
+        <ul>
+          <li>fullstack developer</li>
+          <li>Disponible para trabajar</li>
+        </ul>
+      </div>
     </footer>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import Navbar from './Navbar'
+import mail from '../icons/mail.svg'
+import github from '../icons/github.svg'
 
 export default function Header () {
   return (
@@ -8,8 +10,14 @@ export default function Header () {
       </div>
       <Navbar />
       <div className='social-links'>
-        <a target='_blank' rel='noopener noreferrer' href='https://github.com/Resvier'>GitHub</a>
-        <a target='_blank' rel='noopener noreferrer' href='mailto:javiertf2000@gmail.com'>Email</a>
+        <a target='_blank' rel='noopener noreferrer' href='https://github.com/Resvier'>
+          <img src={github} alt='GitHub' />
+          GitHub
+        </a>
+        <a target='_blank' rel='noopener noreferrer' href='mailto:javiertf2000@gmail.com'>
+          <img src={mail} alt='Email' />
+          Email
+        </a>
 
       </div>
     </header>
