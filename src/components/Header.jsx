de6148bf3a5ec94b4f526beaf5ff033bedb47ec2
Francisco Javier Torres <elodinelarcano@protonmail.com>
2025-09-17 12:18:38 +0200

    added linkdedIn info and updated to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import Navbar from './Navbar'
 import mail from '../icons/mail.svg'
 import github from '../icons/github.svg'
+import linkedin from '../icons/linked-in.svg'
 import { info } from '../data/info'
 
 export default function Header () {
@@ -18,6 +19,10 @@ export default function Header () {
         <a target='_blank' rel='noopener noreferrer' href={`mailto:${info.email}`}>
           <img src={mail} alt='Email' />
           Email
+        </a>
+        <a target='_blank' rel='noopener noreferrer' href={info.linkedin}>
+          <img src={linkedin} alt='LinkedIn' />
+          LinkedIn
         </a>
 
       </div>
--- src/data/info.js
+++ src/data/info.js
@@ -1,6 +1,6 @@
 export const info = {
   email: 'javiertf2000@gmail.com',
   location: 'Lugo, Galicia, España',
-  linkedin: 'https://www.linkedin.com/in/javier-torres-franco-7b2b4b2b4/',
+  linkedin: 'https://www.linkedin.com/in/francisco-javier-torres-franco/',
   github: 'https://github.com/Resvier'
 }
