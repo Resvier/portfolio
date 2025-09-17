bd2598ae1a26e5c06886b8d8922bb165374d4407
Francisco Javier Torres <elodinelarcano@protonmail.com>
2025-09-17 15:24:10 +0200

    Skills section basic styled

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,70 +1,91 @@
+import cursorIcon from '../media/icons/cursor.svg'
+
 export const skills = [
   {
     id: 1,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
     name: 'HTML',
     type: 'frontend'
   },
   {
     id: 2,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
     name: 'CSS',
     type: 'frontend'
   },
 
   {
     id: 3,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
     name: 'JavaScript',
     type: ['frontend', 'language']
   },
 
   {
     id: 4,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
     name: 'React',
     type: 'frontend'
   },
 
   {
     id: 5,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
     name: 'Java',
     type: 'language'
   },
   {
     id: 6,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
     name: 'Python',
     type: 'language'
   },
   {
     id: 7,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqldeveloper/sqldeveloper-original.svg',
     name: 'SQL',
     type: 'language'
   },
   {
     id: 8,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
     name: 'MySQL',
     type: 'database'
   },
   {
     id: 9,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
     name: 'MongoDB',
     type: 'database'
   },
   {
     id: 10,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
     name: 'Git',
     type: 'tools'
   },
   {
     id: 11,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
     name: 'GitHub',
     type: 'tools'
   },
   {
     id: 12,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg',
     name: 'VSCode',
     type: 'tools'
   },
   {
     id: 13,
+    image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg',
     name: 'IntelliJ IDEA',
+    type: 'tools'
+  },
+  {
+    id: 14,
+    image: cursorIcon,
+    name: 'Cursor',
     type: 'tools'
   }
 ]
