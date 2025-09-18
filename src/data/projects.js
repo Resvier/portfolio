35347e1a4c755c1d2a1c26c747b5a474aa311507
Francisco Javier Torres <elodinelarcano@protonmail.com>
2025-09-18 02:49:14 +0200

    updated projects.js info

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,24 +2,24 @@ export const projects = [
   {
     id: 1,
     name: 'Rick and Morty web',
-    description: 'Rick and Morty web is a web application that allows you to search for characters, episodes and locations from the Rick and Morty series.',
+    description: 'Rick and Morty web is a web application that allows you to search for characters, episodes and locations from the series.',
     link: '',
     type: 'frontend',
     technologies: ['HTML', 'CSS', 'JavaScript', 'React'],
     keyFeatures: ['Responsive design', 'Interactive elements', 'Dynamic content'],
     github: 'https://github.com/Resvier/Rick_and_Morty_web',
-    gradient: 'linear-gradient(to right bottom, #a6ffb1, #0b5b00)'
+    gradient: 'linear-gradient(to right bottom, #a6ffb1,rgb(186, 233, 20), #0b5b00)'
   },
   {
     id: 2,
-    name: 'Rick and Morty web',
-    description: 'Rick and Morty web is a web application that allows you to search for characters, episodes and locations from the Rick and Morty series.',
+    name: 'Proyecto en desarrollo . . .',
+    description: 'Proyecto que sera desarrollado proximamente.',
     link: '',
     type: 'frontend',
     technologies: ['HTML', 'CSS', 'JavaScript', 'React'],
     keyFeatures: ['Responsive design', 'Interactive elements', 'Dynamic content'],
-    github: 'https://github.com/Resvier/Rick_and_Morty_web',
-    gradient: 'linear-gradient(to right bottom,rgb(61, 65, 137),rgb(3, 23, 205))'
+    github: '',
+    gradient: 'linear-gradient(to right bottom,rgb(95, 135, 234),rgb(115, 41, 189), #130e8f)'
   }
 
 ]
